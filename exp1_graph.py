afe6f719d780e9a9b7ca83b5d1eb85fd562c037f
ocasoyy <pushkin522@gmail.com>
2021-03-22 13:53:21 +0900

    update

--- exp1_graph.py
+++ exp1_graph.py
@@ -1,26 +1,28 @@
 # Experiments
 import numpy as np
 import pandas as pd
-from typing import Dict
+from typing import Dict, Union
+from collections import Iterable
 
-# from stellargraph.core.indexed_array import IndexedArray
+from stellargraph.core.indexed_array import IndexedArray
 # torch_geometric.data.Data
 
-users =np.array([[-1], [4]])
+users = np.array([[-1], [4]])
 items = np.array([[0.4, 100], [0.1, 200], [0.9, 300]])
-nodes = {'users': users, 'items': items}
-edges = np.array([[0, 0, 0, 1, 1, 2, 3, 3, 4, 4], [2, 3, 4, 3, 4, 0, 0, 1, 0, 1]])
-node_dict = {'user_0': 0, 'user_1': 1, 'item_1': 2, 'item_2': 3, 'item_3': 4}
+node_features = {'user': users, 'item': items}
+edges = np.array([[0, 0, 0, 1, 1, 2, 3, 3, 4, 4],
+                  [2, 3, 4, 3, 4, 0, 0, 1, 0, 1]])
+node_dict = {'user_0': 0, 'user_1': 1, 'item_0': 2, 'item_1': 3, 'item_2': 4}
 
 class Graph(object):
-    def __init__(self, nodes: Dict=None, edges=None, node_dict=None):
-        self.nodes = nodes    # dict
+    def __init__(self, node_features: Dict=None, edges=None, node_dict=None):
+        self.node_features = node_features    # dict
         self.edges = edges
         self.node_dict = node_dict
 
         self.neighbor_dict  = self.get_neighbor_dict()
 
-        if not isinstance(nodes, Dict):
+        if not isinstance(node_features, Dict):
             raise TypeError("nodes must be python dictionary")
 
     def __repr__(self):
@@ -34,14 +36,36 @@ class Graph(object):
     def get_neighbor_dict(self):
         node_list = sorted(list(set(self.edges[0])))
         neighbor_dict = {
-            node_id: edges[:, np.where(edges == node_id)[1]][1].tolist()
+            node_id: edges[:, np.where(edges[0] == node_id)[0]][1].tolist()
             for node_id in node_list
         }
         return neighbor_dict
 
-    def get_neighbors_from_node(self, node):
+    def get_neighbors_from_node(self, node: Union[int, str]) -> list:
+        if type(node) == str:
+            node = self.node_dict[node]
         neighbors = self.neighbor_dict[node]
         return neighbors
+
+    def get_node_type(self, nodes: Union[int, str]) -> list:
+        node_dict = self.node_dict
+
+        # 기본 Input: ['user_1']
+        if not isinstance(nodes, list):
+            nodes = [nodes]
+        if type(nodes[0]) == str:
+            self.check_node_existence(nodes)
+            node_types = [node.split('_')[0] for node in nodes]
+        else:
+            reverse_node_dict = {val: key for key, val in node_dict.items()}
+            nodes = [reverse_node_dict[node] for node in nodes]
+            node_types = [node.split('_')[0] for node in nodes]
+        return node_types
+
+    def check_node_existence(self, nodes: list):
+        existing_nodes = [node for node in nodes if node in list(self.node_dict.keys())]
+        if len(nodes) != len(existing_nodes):
+            raise KeyError("Some of nodes do not exist")
 
     @property
     def num_nodes(self):
@@ -50,21 +74,23 @@ class Graph(object):
 
     @property
     def num_edges(self):
-        num_edges = self.edges.shape[0]
+        num_edges = self.edges.shape[1]
         return num_edges
 
     @property
     def num_user_features(self):
-        num_user_features = self.nodes['user'].shape[1]
+        num_user_features = self.node_features['user'].shape[1]
         return num_user_features
 
     @property
     def num_item_featfures(self):
-        num_item_features = self.nodes['item'].shape[1]
+        num_item_features = self.node_features['item'].shape[1]
         return num_item_features
 
 
-graph = Graph(nodes=nodes, edges=edges, node_dict=node_dict)
+graph = Graph(node_features=node_features, edges=edges, node_dict=node_dict)
+
+
 
 
 
--- exp2_sampler.py
+++ exp2_sampler.py
@@ -2,32 +2,82 @@
 import numpy as np
 import pandas as pd
 from typing import Dict, List
-
-class RandomWalker():
-
-
-
-
+from collections import deque
 
 class Sampler:
     def __init__(
         self,
         Graph,
-        nodes: List=None,
         walk_length: int=None,
-        num_of_walks: int=None,
-        seed=None,
-        walker=None
+        num_of_walks: int=1,
+        seed=None
     ):
         self.graph = Graph
-        self.nodes = nodes
         self.walk_length = walk_length
         self.num_of_walks = num_of_walks
         self.seed = seed
-        self.walker = walker
 
     def __repr__(self):
         return "Sampler Object"
+
+    def run_breadth_first_walk(self, nodes: List=None):
+        walk_length = self.walk_length
+        num_of_walks = self.num_of_walks
+        seed = self.seed
+
+        walks = []
+        depth = 2
+
+        # node = 0 으로 놓고 해보자
+
+        for node in nodes:      # iterate over root nodes
+            for _ in range(num_of_walks):  # do n bounded breadth first walks from each root node
+                queue = list()      # the queue of neighbours
+                walk = list()       # the list of nodes in the subgraph of node
+
+                # Start the walk by adding the head node, and node type to the frontier list q
+                node_type = self.graph.get_node_type(node)[0]
+                queue.extend([(node, node_type, 0)])
+
+                # add the root node to the walks
+                walk.append([node])
+                while len(queue) > 0:
+                    # remove the top element in the queue and pop the item from the front of the list
+                    frontier = queue.pop(0)
+                    current_node, current_node_type, depth = frontier
+                    depth = depth + 1  # the depth of the neighbouring nodes
+
+                    # consider the subgraph up to and including depth d from root node
+                    if depth <= depth:
+                        # Find edge types for current node type
+                        et = self.graph_schema.schema[current_node_type]
+
+                        neighbors = graph.get_neighbors_from_node(current_node)
+                        neigh_et = adj[et][current_node]
+
+                        # If there are no neighbours of this type then we return None
+                        # in the place of the nodes that would have been sampled
+                        # YT update: with the new way to get neigh_et from adj[et][current_node], len(neigh_et) is always > 0.
+                        # In case of no neighbours of the current node for et, neigh_et == [None],
+                        # and samples automatically becomes [None]*n_size[depth-1]
+                        if len(neigh_et) > 0:
+                            samples = rs.choices(neigh_et, k=n_size[depth - 1])
+                        else:  # this doesn't happen anymore, see the comment above
+                            _size = n_size[depth - 1]
+                            samples = [-1] * _size
+
+                        walk.append(samples)
+                        queue.extend(
+                            [
+                                (sampled_node, et.n2, depth)
+                                for sampled_node in samples
+                            ]
+                        )
+
+                # finished i-th walk from node so add it to the list of walks as a list
+                walks.append(walk)
+
+        return walks
 
 
 
@@ -38,6 +88,28 @@ length (int): Length of the walks for the default UniformRandomWalk walker. Leng
 number_of_walks (int): Number of walks from each root node for the default UniformRandomWalk walker.
 seed (int, optional): Random seed for the default UniformRandomWalk walker.
 walker (RandomWalk, optional): A RandomWalk object to use instead of the default UniformRandomWalk walker.
+
+graph_list = {1: set([3, 4]),
+              2: set([3, 4, 5]),
+              3: set([1, 5]),
+              4: set([1]),
+              5: set([2, 6]),
+              6: set([3, 5])}
+root_node = 1
+
+def BFS_with_adj_list(graph, root):
+    visited = []
+    queue = deque([root])
+
+    while queue:
+        n = queue.popleft()
+        if n not in visited:
+            visited.append(n)
+            queue += graph[n] - set(visited)
+    return visited
+  
+print(BFS_with_adj_list(graph_list, root_node))
+
 """
 
 
